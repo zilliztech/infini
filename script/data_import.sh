a292abfb906e50b1ddf8915fb37fdd84f927f0df
jiaxing.zhang <jiaxing.zhang@zilliz.com>
2019-12-02 11:21:36 +0800

    refresh the install script

--- script/data_import.sh
+++ script/data_import.sh
@@ -1,103 +1,53 @@
 #!/bin/bash
-
-if [ $# -eq 1 ];then
-	dir_location=$1
-	megawise_tag=0.4.0
-elif [ $# -eq 2 ];then
-	dir_location=$1
-	megawise_tag=$2
-else
-	echo "Error: please use install_megawise.sh [path(required)] [megawise_tag(optional)] to run it."
-	exit -1
-fi
-
-if [ -d ${dir_location} ];then
-	echo "Error: file /home/zilliz/megawise already exists, please try again."
-  	exit 0
-fi
-
-echo "magawise_tag :" $megawise_tag
-mkdir ${dir_location}/
-docker pull zilliz/megawise:$megawise_tag
-
-if [ -d ${dir_location} ];then
-    echo "Information: installation manual : ${dir_location}."
-else
-    echo "Error: can't create ${dir_location}, please check out the permission."
-    exit -1
-fi
-
-
-
-cp data_import.sh /tmp
-megawise_image_id=$(docker images |grep "zilliz/megawise" | grep "$megawise_tag" \
- |awk '{printf "%s\n",$3}')
-echo "megawise_image_id:" $megawise_image_id
-
-MEGAWISE_CNT=$(docker ps | grep $megawise_image_id | wc -l)
-
-if [ $MEGAWISE_CNT -ne 0 ];then
-	echo "megawise is running..."
-	exit 0
-fi
-
-mkdir ${dir_location}/conf
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/chewie_main.yaml
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/etcd.yaml
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/megawise_config.yaml
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/megawise_config_template.yaml
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/render_engine.yaml
-wget -P ${dir_location}/conf https://raw.githubusercontent.com/Infini-Analytics/infini/master/config/db/scheduler_config_template.yaml
-if [ -f "/tmp/nyc_taxi_data.csv" ];then
-	echo "Warning: /tmp/nyc_taxi_data.csv already exists, you can delete it."
-else
-	wget -P /tmp https://raw.githubusercontent.com/Infini-Analytics/infini/master/sample_data/nyc_taxi_data.csv
-fi
-echo "Information: Configuration parameter"
-echo " 1.egawise username:        MEGAWISE_USER=zilliz"
-echo " 2.megawise password:       MEGAWISE_PWD=zilliz"
-echo " 3.megawise database name:  MEGAWISE_DB=postgres"
-echo " 4.megawise port            MEGAWISE_PORT=5433"
-
-mkdir ${dir_location}/data
-mkdir ${dir_location}/server_data
-docker run --gpus all --shm-size 4294967296 \
- -v ${dir_location}/conf:/megawise/conf  \
- -v ${dir_location}/data:/megawise/data  \
- -v ${dir_location}/server_data:/megawise/server_data  \
- -v $HOME/.nv:/home/megawise/.nv  \
- -v /tmp:/tmp  \
- -p 5433:5432  \
- -d  \
- ${megawise_image_id}
-
-IS_RUN=$(docker ps | grep ${megawise_image_id} | wc -l)
-TRY_CNT=0
-while [ $IS_RUN -eq 0 ];do
+#需要提前将数据导入到 '/tmp'下面
+#create database user and import data
+IS_RUN=$(ps ax | grep "megawise_server" | wc -l)
+while [ $IS_RUN -le 1 ];do
 	sleep 1
-	IS_RUN=$(docker ps | grep ${megawise_image_id} | wc -l)
-	if [ $TRY_CNT -ge 60 ];then
-		echo "Error: start megawise in docker failed, please check it."
-		exit -1
-	fi
-	TRY_CNT=$[$TRY_CNT + 1]
+	IS_RUN=$(ps ax | grep "megawise_server" | wc -l)
 done
 
-echo "State: start megawise in docker successfully!"
 
-container_id=$(docker ps |grep ${megawise_image_id} |awk '{printf "%s\n",$1}')
+IS_RUN=$(ls /megawise/data/ | grep "postmaster.pid" | wc -l)
+while [ $IS_RUN -le 0 ];do
+	sleep 1
+	IS_RUN=$(ls /megawise/data/ | grep "postmaster.pid" | wc -l)
+done
 
-echo "State: copying example data into meagwise.......please wait....."
-docker exec -u megawise -it ${container_id} /tmp/data_import.sh
-if [ $? -ne 0 ]; then
-    echo "Error: import test data failed, you do it by hand using data_import.sh!"
-    exit 0
-else
-	echo "State: Successfully installed MegaWise and imported test data"
-fi
-echo "listen_addresses = '*'" >> ${dir_location}/data/postgresql.conf
-echo "host    all             all             0.0.0.0/0          password" >> ${dir_location}/data/pg_hba.conf
+sleep 5
 
-docker restart ${container_id}
+process_id=$(sed -n '1p' /megawise/data/postmaster.pid)
 
-echo "State: restart megawise successfully! Finished!"
+IS_RUN=$(ps ax | grep postgres |grep ${process_id} | wc -l)
+while [ $IS_RUN -le 0 ];do
+	sleep 1
+	IS_RUN=$(ps ax | grep postgres |grep ${process_id} | wc -l)
+done
+
+sleep 3
+/megawise/bin/psql postgres <<EOF
+CREATE USER zilliz WITH PASSWORD 'zilliz';
+grant all privileges on database postgres to zilliz;
+drop extension if exists zdb_fdw;
+create extension zdb_fdw;
+EOF
+/megawise/bin/psql postgres -U zilliz <<EOF
+drop table if exists nyc_taxi;
+create table nyc_taxi(
+    vendor_id text,
+    tpep_pickup_datetime timestamp,
+    tpep_dropoff_datetime timestamp,
+    passenger_count int,
+    trip_distance float,
+    pickup_longitute float,
+    pickup_latitute float,
+    dropoff_longitute float,
+    dropoff_latitute float,
+    fare_amount float,
+    tip_amount float,
+    total_amount float
+    );
+copy nyc_taxi from '/tmp/nyc_taxi_data.csv'
+ WITH DELIMITER ',' csv header;
+select count(*) from nyc_taxi;
+EOF
